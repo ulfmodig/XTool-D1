b45a56c7fc382538637254437f8ac580ec4a75cb
Ulf Modig <ulf.modig@gmail.com>
2023-03-09 11:51:37 +0100

    First commit

---
+++ boot.py
@@ -0,0 +1,48 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#old: import uos, machine
+#uos.dupterm(None, 1) # disable REPL on UART(0)
+#old: import gc
+#import webrepl
+#webrepl.start()
+#old: gc.collect()
+
+# Complete project details at https://RandomNerdTutorials.com
+
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+import urequests
+import os
+import json
+esp.osdebug(None)
+import gc
+gc.collect()
+
+# Network
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect('modig', 'Dido88bole')
+wlan_mac_address = ubinascii.hexlify(wlan.config('mac')).decode()
+wlan_ip_address = wlan.ifconfig()[0]
+
+# GIT Hub
+github_user = "ulfmodig"
+github_password = "LpQJdgB#s8&6AqD!"
+
+# MQTT
+mqtt_server = '192.168.1.90'
+
+# GPIO
+gpio_13_led = machine.Pin(13, machine.Pin.OUT)
+gpio_12_relay = machine.Pin(12, machine.Pin.OUT)
+
+while wlan.isconnected() == False:
+  pass
+
+
---
+++ main.py
@@ -0,0 +1,89 @@
+def ota_update(path, file)
+
+    # Download
+    full_url = path + "/" + file
+    print("Downloading: " + full_url)    
+    response = urequests.get(full_url, auth=(github_user, github_password))
+    with open('update.bin', 'wb') as file:
+        file.write(response.content)
+        
+    # Update
+    print("Updating: " + file)        
+    if(os.path.isfile(file)):
+        os.remove(file)
+    os.rename("update.bin", file)
+    
+    # Restart
+    restart_and_reconnect()        
+
+def send_mqtt_message(client, topic, msg):
+    print("Publishing topic:%s message:%s" % (topic,msg))
+    client.publish(topic,msg)
+    
+def subscribe_to(client, topic):
+    print("Subscribing to topic: "+topic)
+    client.subscribe(topic)
+    
+def subscribe_callback(topic, msg):
+    print("Received topic:%s message:%s" % (topic, msg))
+    json_doc = json.loads(msg)
+    
+    if json_doc["command"] == "switch:on":
+        gpio_12_relay.value(1)
+    elif json_doc["command"] == "switch:off":
+        gpio_12_relay.value(0)
+    elif json_doc["command"] == "?status":
+        client.publish(device_group, device_name + ':status:' + str(gpio_12_relay.value()))
+    elif json_doc["command"] == "reboot":
+        machine.reset()
+    elif json_doc["command"] == "update":
+        ota_update(json_doc["path"], json_doc["file"])
+
+def connect_and_subscribe():
+    client = MQTTClient(device_name, mqtt_server)
+    client.connect()
+    client.set_callback(subscribe_callback)    
+    subscribe_to(client,device_name)
+    subscribe_to(client,device_group)    
+    return client
+
+def restart_and_reconnect():
+    print('Reconnecting...')
+    time.sleep(10)
+    machine.reset()
+
+devices = {'4cebd6acefc1': 'home/xtool-D1/laser','4cebd6ae22fd': 'home/xtool-D1/air','4cebd6ae2296': 'home/xtool-D1/smoke','4cebd6ae233d': 'home/xtool-D1/enclosure'}
+device_name = devices[wlan_mac_address]
+device_group = 'home/xtool-D1'
+version = "1.01"
+
+try:
+    print("Starting up...")
+    client = connect_and_subscribe()
+    print("Version......: " + version)    
+    print("IP-Address...: " + wlan_ip_address)
+    print("MAC-Address..: " + wlan_mac_address)    
+    print("MQTT-Broker..: " + mqtt_server)
+    print("Device name..: " + device_name)
+    send_mqtt_message(client, device_group, device_name + ":booted:" + version)      
+    print("Started!")
+except OSError as e:
+    restart_and_reconnect()
+
+counter=0
+while True:
+  try:
+    
+    # Check message and blink
+    client.check_msg()
+    time.sleep(1)
+    gpio_13_led.value(1) if gpio_13_led.value() == 0 else gpio_13_led.value(0)
+    
+    # Alive
+    counter += 1
+    if(counter > 60*30):
+        send_mqtt_message(client, device_group, device_name + ":alive")
+        counter = 0
+    
+  except OSError as e:
+    restart_and_reconnect()
