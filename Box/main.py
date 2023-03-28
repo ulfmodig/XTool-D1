8a897a683c546f4b1913604b47132f36d680ffc9
Ulf Modig <ulf.modig@gmail.com>
2023-03-28 15:55:10 +0200

    Updated to 1.04

--- Box/main.py
+++ Box/main.py
@@ -1,4 +1,5 @@
 #  mosquitto_pub -t "home/xtool-D1" -m '{"command": "update", "path": "https://raw.githubusercontent.com/ulfmodig/XTool-D1/master", "file": "test.txt"}'
+#  mosquitto_pub -t "home/xtool-D1/test" -h "192.168.1.90" -m '{\"type\": \"xtool-D1\", \"command\": \"reboot\"}'
 def ota_update(path, filename):
     # Download
     full_url = path + "/" + filename
@@ -21,8 +22,8 @@ def ota_update(path, filename):
 
 def send_command(command):
     global mqtt_client, device_group, device_name, version
-    json = ujson.dumps({"type": "xtool-D1", "device": device_name, "version": version, "command": command,"status": "on" if gpio_12_relay.value() == 0 else "off"})    
-    print("Publishing topic:%s message:%s" % (topic, json))
+    json = ujson.dumps({"type": "xtool-D1", "device": device_name, "version": version, "command": command,"status": "on" if gpio_12_relay.value() == 1 else "off"})    
+    print("Publishing topic:%s message:%s" % (device_group, json))
     mqtt_client.publish(device_group, json)
 
 def subscribe_to(topic):
@@ -40,9 +41,11 @@ def subscribe_callback(topic, msg):
         return
 
     if not "type" in json_doc:
-        pass
-    elif not json_doc["type"] == "xtool-D1"
-        pass
+        print("Ignored, no 'type'!")
+    elif not json_doc["type"] == "xtool-D1":
+        print("Ignored, bad type; " + json_doc["type"])
+    elif not "command" in json_doc:
+        print("Ignored, missing 'command'!")
     elif json_doc["command"] == "switch:on":
         gpio_12_relay.value(1)
         send_command("status")        
@@ -55,16 +58,17 @@ def subscribe_callback(topic, msg):
         machine.reset()
     elif json_doc["command"] == "update":
         ota_update(json_doc["path"], json_doc["file"])
+    else:
+        print("Ignored, unsupported command; " + json_doc["command"])        
 
 
 def connect_and_subscribe():
-    client = MQTTClient(device_name, mqtt_server)
-    client.connect()
-    client.set_callback(subscribe_callback)
-    subscribe_to(client, device_name)
-    subscribe_to(client, device_group)
-    return client
-
+    global mqtt_client
+    mqtt_client = MQTTClient(device_name, mqtt_server)
+    mqtt_client.connect()
+    mqtt_client.set_callback(subscribe_callback)
+    subscribe_to(device_name)
+    subscribe_to(device_group)
 
 def restart_and_reconnect():
     print('Reconnecting...')
@@ -75,14 +79,15 @@ def restart_and_reconnect():
 
 
 devices = {'4cebd6acefc1': 'home/xtool-D1/laser', '4cebd6ae22fd': 'home/xtool-D1/air',
-           '4cebd6ae2296': 'home/xtool-D1/smoke', '4cebd6ae233d': 'home/xtool-D1/enclosure'}
+           '4cebd6ae2296': 'home/xtool-D1/smoke', '4cebd6ae233d': 'home/xtool-D1/enclosure',
+           '4cebd6ae2201': 'home/xtool-D1/test'}
 device_name = devices[wlan_mac_address]
 device_group = 'home/xtool-D1'
-version = "1.03"
+version = "1.04"
 
 try:
     print("Starting up...")
-    mqtt_client = connect_and_subscribe()
+    connect_and_subscribe()
     print("Version......: " + version)
     print("IP-Address...: " + wlan_ip_address)
     print("MAC-Address..: " + wlan_mac_address)
